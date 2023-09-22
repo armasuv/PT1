ef83177dfeea171139e942090b1cc4ab9fd3291a
armasuv <armasuv@educ.college-montreal.qc.ca>
2023-09-22 09:54:26 -0400

    hello black oogway

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 #12 septembre 2023
 
 
-def count_word (phrase):
-    nombre_mot = len(phrase .split())
-    return (nombre_mot)
+def count_word (phrase):  #on crée la fonction count_word
+    nombre_mot = len(phrase .split())   #on donne au programme l'instruction de compter les mots
+    return (nombre_mot)     #on demande au programme de nous retourner la valeur
 
